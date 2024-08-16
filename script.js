ebdf7af8ab60d7e6c9b0c3e8f8fcefd2bfc4bda7
Willian Gustavo <106351072+WillianGustavo@users.noreply.github.com>
2024-08-16 08:11:39 -0300

    site

--- script.js
+++ script.js
@@ -5,27 +5,27 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const caixaResultado = document.querySelector(".caixa-resultado");
 
 const perguntas = [
-    [
-        anunciado: "Na ua opinião
-        alternativas [
-            "Á pratica....
-            "A castração ....
-    ]
- },
- {
-    anunciado: "qual é a visão...."
-    alternativas: [
-        "desafiador.",
-        "apoiam as medidas"
-    ]
-    }
-    {;
-        anunciado:
-        alternativas:
-     ];
+    { 
+        anunciado: "Como as celebrações religiosas influenciam as festas agrícolas e como as práticas agrícolas, por sua vez, impactam as tradições religiosas em diferentes culturas?",
 
+        alternativas: [
+            { 
+
+           texto:"Muitas festividades religiosas estão associadas às colheitas, como a festa de Ação de Graças nos Estados Unidos, que celebra a abundância da agricultura.",
+            afirmacao:"Festas religiosas geralmente não têm relação com práticas agrícolas em sociedades urbanas onde a agricultura não é uma parte central da vida."
+             },
+             {
+                texto:"Algumas tradições agrícolas, como os rituais de plantio e colheita, são moldadas por crenças religiosas e são realizadas para garantir uma boa colheita.",
+                afirmacao:"As práticas agrícolas modernas e as técnicas avançadas na agricultura não têm impacto nas tradições e celebrações religiosas associadas à colheita."
+                 }
+    ]
+}
+];
+ 
      let atual =0;
      let perguntaAtual;
+     let historiaFinal = "";
+
 
      function mostraPergunta(){
         perguntaAtual = perguntas[atual];
@@ -41,5 +41,12 @@ const perguntas = [
             caixaAlternativa.appendChild(botaoAlternativas);
         }
      }
+
+     
+        function respostaSelecionada(opcaoSelecionada) { 
+        const afirmacao = opcaoSelecionada.afirmacao;
+        atual++
+        mostraPergunta();
+      }
 
      mostraPergunta();
