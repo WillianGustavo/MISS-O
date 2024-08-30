9710078dfc4f2229622b8e9c7148a9dc1f0da9f1
Willian Gustavo <106351072+WillianGustavo@users.noreply.github.com>
2024-08-30 07:38:57 -0300

    site

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const caixaPrincipal = document.querySelector(".caixa-principal");
-const caixaPerguntas = document.querySelector(".caixa-pergunta");
-const caixaAlternativa = document.querySelector(".caixa-altrnativas");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
     { 
@@ -22,33 +22,41 @@ const perguntas = [
 }
 ];
  
-     let atual =0;
-     let perguntaAtual;
-     let historiaFinal = "";
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
 
+function mostraPergunta() {
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
+    perguntaAtual = perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
+    textoResultado.textContent = "";
+    mostraAlternativas();
+}
 
-     function mostraPergunta(){
-        perguntaAtual = perguntas[atual];
-        caixaPerguntas.textContent = perguntaAtual.enunciado;
-        caixaAlternativas.textContent = "";
-        mostraAlternativas();
-     }
+function mostraAlternativas(){
+    for(const alternativa of perguntaAtual.alternativas){
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener('click', ()=> respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);        
+    }
+}
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacao = opcaoSelecionada.afirmacao;
+    historiaFinal = afirmacao;
+    atual++
+    mostraPergunta();
+}
 
-     function mostraAlternativas(){
-        for (const alternativas of perguntaAtual.alternativas){
-            const botaoAlternativas = document.createElement("button");
-            botaoAlternativas.textContent = alternativa.texto;
-            botaoAlternativas.addEventListener('click' , () => respostaSelecionada(alternativa));
-            caixaAlternativa.appendChild(botaoAlternativas);
-        }
-     }
+mostraPergunta();
 
-     
-        function respostaSelecionada(opcaoSelecionada) { 
-        const afirmacao = opcaoSelecionada.afirmacao;
-        historiaFinal = afirmacao;
-        atual++
-        mostraPergunta();
-      }
-
-     mostraPergunta();
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2030...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
